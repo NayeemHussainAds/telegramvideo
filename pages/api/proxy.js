d8c5140abc072dc1b33347d083c35b4406f3ff38
Nayeem Hussain <127187936+NayeemHussainAds@users.noreply.github.com>
2026-01-13 22:29:01 +0600

    Update proxy.js

--- pages/api/proxy.js
+++ pages/api/proxy.js
@@ -30,14 +30,14 @@ export default function handler(req, res) {
     fukrey: "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEjV8h2jPZtX_-ShGIm_cnS-Xsapzsb-fBASOYs-x0kcV8lfI_V4sDnnyK-K5flOh3-X-iQrjoZgIMCLYNb7193LGxkqz-77vPYgIGjzfRoCkgtiT8LLvtExoIjZgbeZpMHLVc8kTYKCq1KehWpkYZ5M-SjEUepPLal5L94AsJSXeFpN46nt8GEEkDgzam4/w640-h360/Tiger%20Nageswara%20Rao%20(2).jpg",
     jawan: "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEjNAuW-5150Z6lGflLTsxdC_m5ZGN0-thd_JUYkesGqphqgnc5usYMF792UF8VKfekZSPb8PeUvsQ5n9H2lsLHkZMYJNudBXSZeD__PPpVYPt08Fhsp_t4EV25HtGFhmzBSCMEl0nrJ7QslRNS6oY_Waa0hIbUgrq-m7Ukkhyphenhyphen3gNbgfdfZoRAajvrVU2pPZ/s16000/video-frame-image.png",
     dir: "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEh-qRrfGoK_d4LBL1skUnfADLKMye3Er0Lp5KYVw7QURwiZU65SuQn6KmXA4TstyhS-eC2-BVe_NbHZd8a5rEf7PWJWAT9PSUKzeupD0qpv-aNJ9ja2WWCeS8tooi0Ut3BbjYTKYWQcCZqb0GguGsHmHoYJEFM9vly_jU8aNzFgcLM59gB2S486zVSpuZ7X/w400-h225/edited_thumbnail%20(1).png",
-    marry: "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEj_1E2D6amjZ8Bt-A2ld-vgjK7yE3cBWBTapA2VAST8N34i1f0hKOpICl3UvzXgZy-hriXAAFp_P7iwbiFJ_9_LC6AcufLNdTmOsJ9l7o0j4z8Ph6sIjOf3uw31mX7T0WUQr3QB5dLLc_-Osw-FZjXexRt-b4403Gb_S89Crme3zQF8NhuUk-BNlaOY8u-2/w640-h386/1A7F79D0-A981-47DA-B1E0-D3851824009D-780x470-1.jpeg",
+    marryblur: "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEj6Gs4aSBTBPaAm4csoLmhCP2QAzw9-bztBPmkHJhZHnLYOLBZ4ZVWg7nvLEqg4aqgKK1xkm41KDLyPGefLx4ktT7D5BJjOJZl_wbaWqQWZdGHlJ15wp-yY_nAbi_iUPci_wEciTTzKripmeI5CPC5Fm3E5Q8379sXv-On0y7awGfCJqj-9Itqdqy2YvxIA/w640-h386/final-image.png",
     name: "",
     name: "",
     name: "",
     name: "",
     name: "",
     modi: "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEjTHpN7c4YluAxtGe7yiGv-iReN-VHKaBehp3AGJYYrUM1d8aZUPJ_oPoQUVgmXJLer9ztBmgESzyY7z8RsA6odnqDIx95GrYwqthsuH5Ib16qWqMRaTRVcRh4_hjxwHXtEA_GVVtkzQnzTt0bUDAH_gLqYMV23iEn96A8JxaMNNZRNAiXj6GNuowlyNMlQ/w640-h386/1A7F79D0-A981-47DA-B1E0-D3851824009D-780x470-1.jpeg",
-    marry: "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEj6Gs4aSBTBPaAm4csoLmhCP2QAzw9-bztBPmkHJhZHnLYOLBZ4ZVWg7nvLEqg4aqgKK1xkm41KDLyPGefLx4ktT7D5BJjOJZl_wbaWqQWZdGHlJ15wp-yY_nAbi_iUPci_wEciTTzKripmeI5CPC5Fm3E5Q8379sXv-On0y7awGfCJqj-9Itqdqy2YvxIA/w640-h386/final-image.png"
+    marry: "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEj_1E2D6amjZ8Bt-A2ld-vgjK7yE3cBWBTapA2VAST8N34i1f0hKOpICl3UvzXgZy-hriXAAFp_P7iwbiFJ_9_LC6AcufLNdTmOsJ9l7o0j4z8Ph6sIjOf3uw31mX7T0WUQr3QB5dLLc_-Osw-FZjXexRt-b4403Gb_S89Crme3zQF8NhuUk-BNlaOY8u-2/w640-h386/1A7F79D0-A981-47DA-B1E0-D3851824009D-780x470-1.jpeg"
   };
 
   // 🔍 Query parameter থেকে img মান নেওয়া, না থাকলে default ইমেজ
